e1ddd6439b18845a37d340ee299a9a7058dd2edc
Jochen Topf <jochen@topf.org>
2014-02-19 14:13:38 +0100

    Improved grid with description from README

--- bin/create-grid.sh
+++ bin/create-grid.sh
@@ -16,9 +16,10 @@ rm -f $DATABASE
 
 (cat << EOF
 CREATE TABLE grid (
-    test_id   INTEGER NOT NULL PRIMARY KEY,
-    available INTEGER,
-    result    VARCHAR
+    test_id     INTEGER NOT NULL PRIMARY KEY,
+    available   INTEGER,
+    result      VARCHAR,
+    description VARCHAR
 );
 SELECT AddGeometryColumn('grid', 'geom', 4326, 'POLYGON', 2);
 EOF
@@ -28,12 +29,22 @@ for t in $*; do
         for x in `seq 0 9`; do
             if [ -d data/$t-*/$t$y$x ]; then
                 available=1
-                result=`cat data/$t-*/$t$y$x/result`
+                if [ -f data/$t-*/$t$y$x/result ]; then
+                    result=`cat data/$t-*/$t$y$x/result`
+                else
+                    result=""
+                fi
+                if [ -f data/$t-*/$t$y$x/README ]; then
+                    description=`cat data/$t-*/$t$y$x/README`
+                else
+                    description=""
+                fi
             else
                 available=0
                 result=""
+                description=""
             fi
-            echo "INSERT INTO grid (test_id, geom, available, result) VALUES ($t$y$x, Envelope(LineFromText('LINESTRING(${t}.${x} ${t}.${y},${t}.${x}9999 ${t}.${y}9999)', 4326)), ${available}, '${result}');"
+            echo "INSERT INTO grid (test_id, geom, available, result, description) VALUES ($t$y$x, Envelope(LineFromText('LINESTRING(${t}.${x} ${t}.${y},${t}.${x}9999 ${t}.${y}9999)', 4326)), ${available}, '${result}', '${description}');"
         done
     done
 done) | spatialite $DATABASE >/dev/null 2>&1
