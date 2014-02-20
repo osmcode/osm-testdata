be8b22e96572de98cf291e73538786bd3443aff1
Jochen Topf <jochen@topf.org>
2014-02-20 18:03:37 +0100

    Improved build scripts

--- grid/bin/concat-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-OSMIUM=../../osmium-tool/osmium
-
-rm -f data/all.osm && $OSMIUM cat data/*/*/*.osm -o data/all.osm
-
--- grid/bin/show-tests.sh
+++ grid/bin/show-tests.sh
@@ -3,5 +3,25 @@
 #  show-tests.sh
 #
 
-find data -mindepth 2 -maxdepth 2 -type d | sort | cut -d/ -f2,3 | sed -e 's/\// /' | sed -e 's/-/ /'
+set -e
+
+tests=`find data -mindepth 2 -maxdepth 2 -type d | sed -s 's/data\///' | sort`
+
+for t in $tests; do
+    cat_no=${t%%-*}
+    cat_name_test_id=${t#*-}
+    cat_name=${cat_name_test_id%/*}
+    test_id=${t#*/}
+    if [ -f data/$t/README ]; then
+        description=`cat data/$t/README`
+    else
+        description="NO README"
+    fi
+    if [ -f data/$t/result ]; then
+        result=`cat data/$t/result`
+    else
+        result="UNKNOWN RESULT"
+    fi
+    echo "$cat_no $cat_name\t$test_id\t$result\t$description"
+done
 
