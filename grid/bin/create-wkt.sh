95fd67b075b62da2e813f45dca1cae6f67a57bf9
Jochen Topf <jochen@topf.org>
2014-02-25 10:31:18 +0100

    Remove duplicate coordinates when generating ways.wkt file

--- grid/bin/create-wkt.sh
+++ grid/bin/create-wkt.sh
@@ -4,16 +4,23 @@
 #
 #  This script will generate possible nodes.wkt and ways.wkt files
 #  from the data.osm file. YOU HAVE TO CHECK THE OUTPUT, BECAUSE
-#  THIS SCRIPT WILL NOT ALWAYS DO THE RIGHT THING!
+#  THIS SCRIPT MIGHT NOT ALWAYS DO THE RIGHT THING!
 #
 
 for t in data/*/*; do
     xsltproc bin/osm2nodes_wkt.xsl $t/data.osm >$t/nodes.wkt
+
     if [ ! -s $t/nodes.wkt ]; then
         rm $t/nodes.wkt
     fi
 
-    xsltproc bin/osm2ways_wkt.xsl $t/data.osm >$t/ways.wkt
+    xsltproc bin/osm2ways_wkt.xsl $t/data.osm | \
+        # remove duplicate coordinates
+        sed -re 's/([0-9.]+ [0-9.]+),\1/\1/' | \
+        # remove linestrings with single coordinate
+        egrep -v 'LINESTRING\([0-9.]+ [0-9.]+\)' \
+        >$t/ways.wkt
+
     if [ ! -s $t/ways.wkt ]; then
         rm $t/ways.wkt
     fi
