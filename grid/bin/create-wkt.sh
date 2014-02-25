720ce36f4560258fa39e441cc9b5c38a0758846f
Jochen Topf <jochen@topf.org>
2014-02-25 10:02:38 +0100

    Add scripts to generate (nodes|ways).wkt from data.osm
    This will not always to the right thing, so you have to check the output.

---
+++ grid/bin/create-wkt.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+#  create-wkt.sh
+#
+#  This script will generate possible nodes.wkt and ways.wkt files
+#  from the data.osm file. YOU HAVE TO CHECK THE OUTPUT, BECAUSE
+#  THIS SCRIPT WILL NOT ALWAYS DO THE RIGHT THING!
+#
+
+for t in data/*/*; do
+    xsltproc bin/osm2nodes_wkt.xsl $t/data.osm >$t/nodes.wkt
+    if [ ! -s $t/nodes.wkt ]; then
+        rm $t/nodes.wkt
+    fi
+
+    xsltproc bin/osm2ways_wkt.xsl $t/data.osm >$t/ways.wkt
+    if [ ! -s $t/ways.wkt ]; then
+        rm $t/ways.wkt
+    fi
+done
+
