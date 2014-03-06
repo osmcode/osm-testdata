f8d7193156af0f529281a0af67faca0432db0a6b
Jochen Topf <jochen@topf.org>
2014-03-06 19:28:12 +0100

    Improve geometry comparison scripts

--- bin/compare-wkt.sh
+++ bin/compare-wkt.sh
@@ -1,56 +1,86 @@
 #!/bin/sh
 #
-#  compare-wkt.sh WKT1 WKT2
+#  compare-wkt.sh WKT_REF WKT_TEST
 #
-#  Compares the two geometries given on the command line.
+#  Compares the reference and the test geometries given on the command line
+#  in WKT format.
 #
-#  Outputs "OK" or "ERR" to show whether the test succeeded or failed together
-#  with some more information in parenthesis.
+#  Outputs a line to STDOUT ending in "OK" or "ERR" to show whether the test
+#  succeeded or failed. More detailed information is written to STDERR.
 #
 #  Returns
-#  0 - if they are identical
-#  1 - if they are geometrically equivalent
-#  2 - if they are different
-#  3 - at least one of the geometries is broken
+#  0 - if the geometries are identical
+#  1 - if the geometries are geometrically equivalent
+#  2 - if the geometries are different
+#  3 - if at least one of the geometries is broken
 #
 #  Instead of a WKT geometry the string "INVALID" can also be given which only
 #  compares equal to another "INVALID".
 #
 
 set -e
+#set -x
 
-WKT1=$1
-WKT2=$2
+WKT_REF=$1
+WKT_TEST=$2
 
-OK="OK"
-ERR="ERR"
+DATABASE=compare-wkt-tmp-$$.db
 
-#OK="\033[1;32mOK\033[0m"
-#ERR="\033[1;31mERR\033[0m"
-
-if [ "$WKT1" = "$WKT2" ]; then
-    echo "$OK  (identical)"
+if [ "$WKT_REF" = "INVALID" -a "$WKT_TEST" = "INVALID" ]; then
+    echo "both INVALID => OK"
     exit 0
 fi
 
-if [ "$WKT1" = "INVALID" -o "$WKT2" = "INVALID" ]; then
-    echo "$ERR (one is invalid)"
+if [ "$WKT_REF" = "$WKT_TEST" ]; then
+    echo "geoms identical => OK"
+    exit 0
+fi
+
+if [ "$WKT_REF" = "INVALID" ]; then
+    echo "should be INVALID => ERR"
     exit 2
 fi
 
-result=`echo "SELECT Equals(GeomFromText('$WKT1', 4326), GeomFromText('$WKT2', 4326));" | spatialite -batch -bail tmp$$.db 2>/dev/null | tail -1`
-rm -f tmp$$.db
+if [ "$WKT_TEST" = "INVALID" ]; then
+    echo "should not be INVALID => ERR"
+    exit 2
+fi
+
+echo "Testing reference WKT [$WKT_REF]:" 1>&2
+rwkt_ref=`spatialite -batch -bail $DATABASE "SELECT IsValid(GeomFromText('$WKT_REF', 4326));" | tail -1`
+if [ "$rwkt_ref" != "1" ]; then
+    echo "reference geometry is invalid => ERR"
+    echo "  Reference geometry is invalid. result: $rwkt_ref" 1>&2
+    rm -f $DATABASE
+    exit 3
+fi
+
+echo "  Geometry valid" 1>&2
+
+echo "Testing test WKT [$WKT_TEST]:" 1>&2
+rwkt_test=`spatialite -batch -bail $DATABASE "SELECT IsValid(GeomFromText('$WKT_TEST', 4326));" | tail -1`
+if [ "$rwkt_test" != "1" ]; then
+    echo "test geometry is invalid => ERR"
+    echo "  Test geometry is invalid. result: $rwkt_test" 1>&2
+    rm -f $DATABASE
+    exit 3
+fi
+
+echo "  Geometry valid" 1>&2
+
+result=`spatialite -batch -bail $DATABASE "SELECT Equals(GeomFromText('$WKT_REF', 4326), GeomFromText('$WKT_TEST', 4326));" | tail -1`
+rm -f $DATABASE
 
 if [ "$result" = "1" ]; then
-    echo "$OK  (equal)"
+    echo "geoms equal => OK"
     exit 1
 fi
 
 if [ "$result" = "0" ]; then
-    echo "$ERR (different)"
+    echo "geoms different => ERR"
     exit 2
 fi
 
-echo "$ERR (failure)"
+echo "unknown failure => ERR"
 exit 3
 
