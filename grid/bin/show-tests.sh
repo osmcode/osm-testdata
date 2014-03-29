0c813759fcc8a73ca509ed5c843d27aba9886460
Jochen Topf <jochen@topf.org>
2014-03-29 22:44:54 +0100

    Remove description.txt files (now in test.json)

--- grid/bin/show-tests.sh
+++ grid/bin/show-tests.sh
@@ -10,16 +10,11 @@ tests=`find data -mindepth 2 -maxdepth 2 -type d | sed -s 's/data\///' | sort`
 for t in $tests; do
     cat_no=${t%%/*}
     test_id=${t#*/}
-    if [ -f data/$t/description.txt ]; then
-        description=`cat data/$t/description.txt`
-    else
-        description="NO DESCRIPTION"
-    fi
     if [ -f data/$t/result ]; then
         result=`cat data/$t/result`
     else
         result="UNKNOWN RESULT"
     fi
-    printf "%1s %3s %-7s %s\n" $cat_no $test_id $result "$description"
+    printf "%1s %3s %-7s %s\n" $cat_no $test_id $result
 done
 
